1b9e90b0e6a1b179bf646a975c1dd80e09f7e220
simran-codes05 <simranneman7@gmail.com>
2026-03-23 22:12:46 +0530

    Improved JSON storage without overwriting the data

--- My_1st_automation_code/Main.py
+++ My_1st_automation_code/Main.py
@@ -1,11 +1,18 @@
 import json
-Student_data = []
+
+#If file already exists
+try:
+    with open("Student_data.json", "r") as file:
+        Student_data = json.load(file)
+except FileNotFoundError:
+        Student_data = []
 
 # take student data from user
 name = input("Enter student name:")
 marks = int(input("Enter student marks:"))
 
 new_student = {"name" : name, "marks" : marks}
+# now its add new student to existing data
 Student_data.append(new_student)
 
 #saving the data to JSON file
@@ -13,13 +20,14 @@ with open("Student_data.json","w") as file:
     json.dump(Student_data, file,indent = 4)
 
 #read the existing data from json file
-with open("Student_data.json","r") as file:
-    students = json.load(file)
+#with open("Student_data.json","r") as file:
+ #   students = json.load(file)
 
 #Filter student data as marks > 50
-print("Students with marks > 50:\n")
+print("\n Students with marks > 50:\n")
 
-for s in students:
+for s in Student_data:
     if s["marks"] > 50:
-        print(f"Name: {s['name']},Marks: {s['marks']}")
+        print(f"Name: {s['name']} , Marks: {s['marks']}")
+
         
